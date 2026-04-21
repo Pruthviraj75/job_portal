fc0c265d8eebdc6940a0e81944c6977b1d8178ac
Pruthviraj <gaikwadpruthviraj01@gmail.com>
2026-04-22 01:00:09 +0530

    update: fixed bugs and improved backend

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -5,14 +5,19 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const useGetAllJobs = () => {
-  const {searchedQuery} = useSelector(store=>store.job)
+  const { searchedQuery } = useSelector((store) => store.job);
   const dispatch = useDispatch();
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
-        const res = await axios.get(`${Job_API_ENDPOINT}/get?keyword=${searchedQuery}`, { withCredentials: true });
-        if (res.data.success) {
-          dispatch(setAllJobs(res?.data?.jobs));
+        const res = await axios.get(
+          `${Job_API_ENDPOINT}/get?keyword=${searchedQuery}`,
+          { withCredentials: true },
+        );
+        if (res?.data?.success) {
+          dispatch(setAllJobs(res.data.jobs));
+        } else {
+          dispatch(setAllJobs([])); // fallback
         }
       } catch (error) {
         console.log(error);
